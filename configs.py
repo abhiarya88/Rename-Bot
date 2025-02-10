aee311b7cad223de5f75ca885f55b660c91c37f2
abhiarya88 <harishnaik4488@gmail.com>
2025-02-10 22:15:41 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -14,10 +14,10 @@ logging.basicConfig(
 class Config(object):
     API_ID = int(os.environ.get("API_ID", "25576719"))
     API_HASH = os.environ.get("API_HASH", "cecb3ae504b6dd120115662bc839d16c")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7502614346:AAGvgehU008cpJMzWRce8khPyvwhRhEt3dY")
     DOWNLOAD_DIR = os.environ.get("DOWNLOAD_DIR", "./downloads")
     LOGGER = logging
-    OWNER_ID = int(os.environ.get("OWNER_ID", 1445283714))
+    OWNER_ID = int(os.environ.get("OWNER_ID", 7142188712))
     PRO_USERS = list(set(int(x) for x in os.environ.get("PRO_USERS", "0").split()))
     PRO_USERS.append(OWNER_ID)
     MONGODB_URI = os.environ.get("MONGODB_URI", "mongodb+srv://mawoh51500:45fKeeuqhg4yeVPu@cluster0.xhjz5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
